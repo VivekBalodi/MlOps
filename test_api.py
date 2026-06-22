4796117ecd8815f9777a87511cefb86b141f3342
vivek <vivek@gmail.com>
2026-06-22 10:17:52 +0530

    first commit

---
+++ test_api.py
@@ -0,0 +1,12 @@
+{
+  "Area": 69647,
+  "Perimeter": 1084.318,
+  "Major_Axis_Length": 462.9416,
+  "Minor_Axis_Length": 191.821,
+  "Convex_Area": 70216,
+  "Equiv_Diameter": 297.7874,
+  "Solidity": 0.9919,
+  "Roundness": 0.6002,
+  "Compactness": 0.7444,
+  "Shape_Factor_1": 0.6433
+}
